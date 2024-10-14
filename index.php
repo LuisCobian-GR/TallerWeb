b12f79a655797bb2a1f0cda5a9baa26b467a301f
Luis Cobian <coby10@gmail.com>
2024-10-14 12:57:24 -0600

    Hasta el 14 de octubre

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <html>
     <head>
         <title>Tienda x</title>
+        <link href="css/main.css" rel="stylesheet" />
     </head>
     <body bgcolor="#928333">
+        <div class="contenedor">
+            <nav>
+                <div class="encabezado">
+                    <img src="images/logo_tienda.png" />
+                    <div class="user-titulo">Luis Cobian</div>
+                    <div class="rol-titulo">Administrador</div>
+                    <div class="fecha-titulo">00/00/00 00:00</div>
+                </div>
+                <div class="enlaces">
+                </div>
+                <div class="firma">
+                </div>
+                
+            </nav>
+        </div>
         <h1> Bienvenidos </h1>
         <a href="productos.php">
             Ve a productos
