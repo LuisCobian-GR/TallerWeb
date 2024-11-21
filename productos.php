1f98469d62040162811c6062d361a9274042676e
Luis Cobian <coby10@gmail.com>
2024-11-21 12:54:48 -0600

    Hasta el dia 21 de nov, acomodo del catalogo

--- productos.php
+++ productos.php
@@ -10,23 +10,39 @@
     include("encabezado.php");
 ?>
 
-    <a href="index.php"> regresar </a>  
-    <br> <br/> <!-- Aqui me permita mandar a productos --> 
-    <a href="agregar_producto.php">Agregar producto</a>
-    <br ><br> 
-    <?php
+    <h1> Catalogo de productos </h1>
+    
+    <div class="productos_contenedor">
+        <!-- Titulo de mini ventana -->
+        <div class="titulo">
+            <h3>Inventario </h3>
+        </div>
+        <!-- Van los productos --> 
+         <div class="productos_catalogo">
+         <?php
         foreach($datos as $producto){
            ?>
-                 <div> 
-                    <img src="images/logo_tienda.png" />
-                    <br>
+                 <div class="producto"> 
+                    <div class="img">
+                        <img src="images/logo_tienda.png" />
+                    </div>
                     <h3> <?= $producto['nombre'] ?> </h3>
                     <h4> <?= $producto['categoria'] ?> </h4>
                     <p> <?= $producto['precio'] ?> </p>
                  </div>
-           <?php 
-        }
-    ?> 
+                <?php 
+                }
+            ?> 
+         </div>
+
+
+    </div>
+
+    <a href="index.php"> regresar </a>  
+    <br> <br/> <!-- Aqui me permita mandar a productos --> 
+    <a href="agregar_producto.php">Agregar producto</a>
+    <br ><br> 
+    
 
 <?php 
     include("piepagina.php"); 
